69f90ddac002ad5c6fe807b515d04fd3167e875b
mahajansayali <136877654+mahajansayali@users.noreply.github.com>
2024-07-22 20:25:45 +0530

    Add files via upload
    Swapping of 2 numbers using third variable

---
+++ SwapNo.java
@@ -0,0 +1,17 @@
+class SwapNo
+{
+	public static void main(String args[])
+	{
+		
+ 		int a=10;
+		int b=20;
+		int c;
+		System.out.println("Before Swapping the value of a : "+a);
+		System.out.println("Before Swapping the value of b : "+b);
+		c=a;
+		a=b;
+		b=c;
+		System.out.println("After Swapping the value of a : "+a);
+		System.out.println("After Swapping the value of b : "+b);
+	}
+}
