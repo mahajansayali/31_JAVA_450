13d5b5a2d2344ad7964fa001b78d0570005f4966
mahajansayali <136877654+mahajansayali@users.noreply.github.com>
2024-08-08 08:57:04 +0530

    Add files via upload
    Collection

---
+++ JavaCollection.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class JavaCollection
+{
+	public static void main(String arg[])
+	{
+		ArrayList<String>list=new ArrayList<String>();
+		list.add("Kadu");
+		list.add("Bhayati");
+		list.add("Akshu");
+		list.add("Ankita");
+		System.out.println(list);
+		
+	}
+}
